81fcf5e6887c0714d10c52219bbdec9ef2bc25f1
a-tomono <a.tomono@gmail.com>
2020-06-30 17:17:54 +0900

    テストのリファクタリング

--- src/test/groovy/com/example/springbootpractice/domain/ContractSpec.groovy
+++ src/test/groovy/com/example/springbootpractice/domain/ContractSpec.groovy
@@ -2,30 +2,65 @@ package com.example.springbootpractice.domain
 
 import spock.lang.Specification
 
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
+
 
 class ContractSpec extends Specification {
 
-    def "ドラフトをレビューする"(){
+    def "作成した契約を編集して下書きする"() {
         given:
-        def contract = new Contract();
-        contract.draft();
+        def contract = new Contract("name", "abst")
+        def modifiedName = "drafted name"
+        def modifiedAbstract = "drafted abst"
 
         expect:
-        contract.review();
-        contract.isReviewed();
-
+        contract.draft(modifiedName, modifiedAbstract)
+        contract.getContractName().equals(modifiedName)
+        contract.getContractAbstract().equals(modifiedAbstract)
+        contract.isDraft()
     }
 
-    def "初期状態はレビューできない"(){
-        setup:
-        def contract = new Contract();
+    def "複数回下書きすると日付が更新される"() {
+        given:
+        def contract = new Contract()
+        def modifiedName = "name"
+        def anotherModifiedName = "another name"
+        def modifiedAbstract = "abst"
+        def anotherModifiedAbstract = "another abst"
 
         when:
-        contract.review();
+        contract.draft(modifiedName, modifiedAbstract)
+        def firstDraftDateTime = contract.getDraftDate()
+        contract.draft(anotherModifiedName, anotherModifiedAbstract)
 
         then:
-        def e = thrown(RuntimeException.class);
-        e.class == RuntimeException.class;
-        e.message == "can not review this contract";
+        contract.getContractName().equals(anotherModifiedName)
+        contract.getContractAbstract().equals(anotherModifiedAbstract)
+        contract.getDraftDate().isAfter(firstDraftDateTime)
+    }
+
+    def "下書きをレビューする"() {
+        given:
+        def contract = new Contract()
+        contract.draft("draftName", "draftAbstract")
+
+        expect:
+        contract.review()
+        contract.isReviewed()
+    }
+
+    def "作成した初期状態はレビューできない"() {
+        setup:
+        def contract = new Contract()
+
+        when:
+        contract.review()
+
+        then:
+        def e = thrown(RuntimeException.class)
+        e.class == RuntimeException.class
+        e.message == "can not review this contract"
     }
 }
